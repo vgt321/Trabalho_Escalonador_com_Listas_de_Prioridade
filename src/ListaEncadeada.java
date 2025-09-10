a051463fa61c3681718eb09768f10c16f89bfe33
Mardson Varela Lima <mardsonlima1379@gmail.com>
2025-09-09 21:50:42 -0300

    1.00.02

--- src/ListaEncadeada.java
+++ src/ListaEncadeada.java
@@ -1,7 +1,10 @@
 public class ListaEncadeada {
 
         lista.inserirNoFim(new Processo (1, "ProcA", 2, 5, "Disco"));
-        
+        lista.inserirNoFim(new Processo(2, "ProcB", 1, 3, "Impressora"));
+        lista.inserirNoFim(new Processo(3, "ProcC", 3, 7, "Rede"));
+        System.out.println("Lista após inserções:");
+        lista.exibir();
     }
 
 }
